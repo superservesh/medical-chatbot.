03cbbc8d01923eba94e4f439c9d4ca9d6e9012dc
superservesh <sarveshkumar210303@gmail.com>
2026-07-04 12:47:40 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="sarvesh kumar",
+    author_email="sarveshkumar210303@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
